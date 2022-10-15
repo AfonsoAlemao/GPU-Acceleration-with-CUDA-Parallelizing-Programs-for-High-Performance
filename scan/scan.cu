16abfeb27754824b437158ce01622490f2bb1373
Rpcd <67294809+RuiDaniel@users.noreply.github.com>
2022-10-15 19:48:18 +0100

    Add files via upload

--- scan/scan.cu
+++ scan/scan.cu
@@ -14,6 +14,19 @@
 
 #define THREADS_PER_BLOCK 256
 
+#define DEBUG
+#ifdef DEBUG
+#define cudaCheckError(ans) { cudaAssert((ans), __FILE__, __LINE__); }
+inline void cudaAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+if (code != cudaSuccess) {
+fprintf(stderr, "CUDA Error: %s at %s:%d\n", cudaGetErrorString(code), file, line);
+if (abort) exit(code);
+}
+}
+#else
+#define cudaCheckError(ans) ans
+#endif
 
 // helper function to round an integer up to the next power of 2
 static inline int nextPow2(int n) {
@@ -448,17 +461,3 @@ void printCudaInfo()
     }
     printf("---------------------------------------------------------\n"); 
 }
-
-#define DEBUG
-#ifdef DEBUG
-#define cudaCheckError(ans) { cudaAssert((ans), __FILE__, __LINE__); }
-inline void cudaAssert(cudaError_t code, const char *file, int line, bool abort=true)
-{
-if (code != cudaSuccess) {
-fprintf(stderr, "CUDA Error: %s at %s:%d\n", cudaGetErrorString(code), file, line);
-if (abort) exit(code);
-}
-}
-#else
-#define cudaCheckError(ans) ans
-#endif
