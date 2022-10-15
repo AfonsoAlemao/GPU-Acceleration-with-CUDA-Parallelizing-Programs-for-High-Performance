f142de3f3895f805158334c2153f6fef7d425ff0
Rpcd <67294809+RuiDaniel@users.noreply.github.com>
2022-10-15 19:48:31 +0100

    Add files via upload

--- saxpy/saxpy.cu
+++ saxpy/saxpy.cu
@@ -6,6 +6,20 @@
 
 #include "CycleTimer.h"
 
+#define DEBUG
+#ifdef DEBUG
+#define cudaCheckError(ans) { cudaAssert((ans), __FILE__, __LINE__); }
+inline void cudaAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+if (code != cudaSuccess) {
+fprintf(stderr, "CUDA Error: %s at %s:%d\n", cudaGetErrorString(code), file, line);
+if (abort) exit(code);
+}
+}
+#else
+#define cudaCheckError(ans) ans
+#endif
+
 
 // return GB/sec
 float GBPerSec(int bytes, float sec) {
@@ -150,16 +164,3 @@ void printCudaInfo() {
     printf("---------------------------------------------------------\n");
 }
 
-#define DEBUG
-#ifdef DEBUG
-#define cudaCheckError(ans) { cudaAssert((ans), __FILE__, __LINE__); }
-inline void cudaAssert(cudaError_t code, const char *file, int line, bool abort=true)
-{
-if (code != cudaSuccess) {
-fprintf(stderr, "CUDA Error: %s at %s:%d\n", cudaGetErrorString(code), file, line);
-if (abort) exit(code);
-}
-}
-#else
-#define cudaCheckError(ans) ans
-#endif
