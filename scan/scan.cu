686a92c10abcfb6d9841999b1b7347c2256d23e4
Rpcd <67294809+RuiDaniel@users.noreply.github.com>
2022-10-16 22:45:40 +0100

    Add files via upload

--- scan/scan.cu
+++ scan/scan.cu
@@ -51,12 +51,12 @@ upsweepPhaseKernel(int twod1, int twod, int* result, int N) {
     // calculation is needed so the code only looks at the .x terms of
     // blockDim and threadIdx.
     int index = blockIdx.x * blockDim.x + threadIdx.x;
-    // if (index < N / twod1) {
+    if (index < N / twod1) {
         index *= twod1;
         if (index + twod1 - 1 < N) {
             result[index + twod1 - 1] = result[index + twod - 1] + result[index + twod1 - 1];
         }
-    // }
+    }
 
 }
 
@@ -70,7 +70,7 @@ downsweepPhaseKernel(int twod1, int twod, int* result, int N, int nextPow2var) {
     // calculation is needed so the code only looks at the .x terms of
     // blockDim and threadIdx.
     int index = blockIdx.x * blockDim.x + threadIdx.x;
-    // if (index < nextPow2var / twod1) {
+    if (index < nextPow2var / twod1) {
         index *= twod1;
         if (index + twod1 - 1 < nextPow2var) {
             int aux = result[index + twod1 - 1];
@@ -80,7 +80,7 @@ downsweepPhaseKernel(int twod1, int twod, int* result, int N, int nextPow2var) {
             }
             result[index + twod - 1] = aux;
         }
-    // }
+    }
 }
 
 
